b18746bf29b59eaccca1c5ad700cb21b46f2b23a
Viktar Vedmich <viktar_vedmich@epam.com>
2019-01-14 23:31:14 +0300

    Fix prounounce

--- testscript.sh
+++ testscript.sh
@@ -4,4 +4,4 @@ NUMBER=$(($NUMBERONE + $NUMBERTWO))
 echo "$NUMBER"
 echo "test message for pull command on jenkins"
 echo "One more test message"
-echo "this was run automagically"
+echo "this was run automatically"
