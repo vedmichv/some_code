b1fe4fe60a2ed3d64857fbbb7dc4fa4ba58e008f
Viktar Vedmich <viktar_vedmich@epam.com>
2019-01-14 23:19:52 +0300

    One more test message

--- testscript.sh
+++ testscript.sh
@@ -3,4 +3,5 @@ echo "this script is from github"
 NUMBER=$(($NUMBERONE + $NUMBERTWO))
 echo "$NUMBER"
 echo "test message for pull command on jenkins"
+echo "One more test message"
 echo "this was run automagically"
